3e4845e00ed8e2fa0a643828fc70329184cc2568
Satyamsinghhhh <satyamsingh95863@gmail.com>
2026-01-29 10:30:24 +0530

    new files

---
+++ java coding/percentage.java
@@ -0,0 +1,23 @@
+public class percentage{
+    public static void main (String[] args){
+
+        int maths = 45;
+        int cs = 44;
+        int java = 41;
+        int sanskrit = 42;
+        int rdbms = 41;
+    int total = maths + cs + java + sanskrit + rdbms;
+int maxinumber = 50 * 5;
+float percentage =  (total * 100)/maxinumber;
+
+    System.out.println("The total marks in maths is :" + maths);
+        System.out.println("The total marks in cs is :" + cs);
+    System.out.println("The total marks in java is :" + java);
+    System.out.println("The total marks in sanskrit is :" + sanskrit);
+    System.out.println("The total marks in rdbms is :" + rdbms);
+        System.out.println("The total percentage is  :" + percentage);
+
+
+}
+
+}
